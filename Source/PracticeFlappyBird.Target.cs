783c4f4903e976a43bf43a00345b1e8282666e2c
feedallcat <feedallcat@gmail.com>
2026-02-09 23:22:23 +0700

    update to UE 5.7

--- Source/PracticeFlappyBird.Target.cs
+++ Source/PracticeFlappyBird.Target.cs
@@ -8,8 +8,8 @@ public class PracticeFlappyBirdTarget : TargetRules
 	public PracticeFlappyBirdTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("PracticeFlappyBird");
 	}
 }
